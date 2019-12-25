d6e7f3301e228d4d4a75fc1c7d83515377352ecc
James Cole <thegrumpydictator@gmail.com>
2019-12-25 13:13:03 +0100

    Makre sure storage directories exist and generate keys.

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+
+# make directories in storage.
+mkdir -p /var/www/storage/app/public
+mkdir -p /var/www/storage/framework/cache
+mkdir -p /var/www/storage/framework/sessions
+mkdir -p /var/www/storage/framework/testing
+mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/logs
+
+# run migration.
 cd /var/www
+php artisan passport:keys
 php artisan migrate --seed
 php /init.php
